8ce49e738bfd52ea287eb519abfdedf5c7e505d1
justerest <justerest@gmail.com>
2022-05-11 23:49:44 +0500

    fix: logo

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: './logo.component.svg',
+  templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
